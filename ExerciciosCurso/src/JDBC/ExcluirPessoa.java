e3d52194ccb543f9d263075cc2c25914882eb49f
LizRabacal <lizrabacala@gmail.com>
2024-06-14 14:57:48 -0300

    Atualização

--- ExerciciosCurso/src/JDBC/ExcluirPessoa.java
+++ ExerciciosCurso/src/JDBC/ExcluirPessoa.java
@@ -10,35 +10,25 @@ import JDBC.Conexao;
 public class ExcluirPessoa {
 	public static void main(String[] args) throws SQLException {
 
+		Connection conexao = Conexao.getConexao();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o id de quem vc quer excluir: ");
+		int id = sc.nextInt();
 
-		//Desafio pra fazer um mecanismo de atualizar registro no banco de dados
+		String str = """
+				DELETE FROM PESSOAS WHERE CODIGO = ?;
+				""";
 
+		PreparedStatement stmt = conexao.prepareStatement(str);
+		stmt.setInt(1, id);
 
-				
-				Connection conexao = Conexao.getConexao();
-				Scanner sc = new Scanner(System.in);
-				System.out.println("Digite o id de quem vc quer excluir: ");
-				int id = sc.nextInt();
-		
-
-				String str = """
-						DELETE FROM PESSOAS WHERE CODIGO = ?;
-						""";
-
-					PreparedStatement stmt = conexao.prepareStatement(str);
-					stmt.setInt(1, id);
-					
-					if(stmt.executeUpdate() >0) {
-						System.out.println("Registro deletado com sucesso");
-					}else {
-						System.out.println("Nao foi deletado :(");
-					}
-				conexao.close();
-				sc.close();
-			}
-
-		
-
-	
+		if (stmt.executeUpdate() > 0) {
+			System.out.println("Registro deletado com sucesso");
+		} else {
+			System.out.println("Nao foi deletado :(");
+		}
+		conexao.close();
+		sc.close();
+	}
 
 }
